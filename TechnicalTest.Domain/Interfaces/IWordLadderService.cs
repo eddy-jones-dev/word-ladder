400169742f5c0939e0808534a9b99b2120b346b9
Eddy Jones <eddyjones1982@gmail.com>
2021-08-06 22:03:00 +0100

    create service interface

---
+++ TechnicalTest.Domain/Interfaces/IWordLadderService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TechnicalTest.Domain.Services;
+
+namespace TechnicalTest.Domain.Interfaces
+{
+    public interface IWordLadderService
+    {
+        List<string> GetNextWords(string startWord, List<string> validWords);
+        List<string> FilterValidWords(string[] dictionary);
+        int GetDistance(List<string> words);
+        List<Node> CreateGraph(string startWord, string endWord, List<string> validWords);
+        int GetNumberOfChanges(List<Node> graph);
+    }
+}
--- TechnicalTest.Domain/Node.cs
+++ TechnicalTest.Domain/Node.cs
@@ -1,9 +1,12 @@
-﻿namespace TechnicalTest.Domain.Services
+﻿using System.Collections.Generic;
+
+namespace TechnicalTest.Domain.Services
 {
         public class Node
         {
             public int Level { get; set; }
             public string ParentNode { get; set; }
             public string ChildNode { get; set; }
+            public List<string> NodePath { get; set; }
         }
 }
