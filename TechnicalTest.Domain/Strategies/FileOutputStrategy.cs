4f2d1896fc73ec1bfed12b5e8489792bc8d25a76
eddy-jones-dev <eddyjones1982@gmail.com>
2021-08-09 08:24:37 +0100

    use file service in output strategy

--- TechnicalTest.Domain/Strategies/FileOutputStrategy.cs
+++ TechnicalTest.Domain/Strategies/FileOutputStrategy.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.IO;
 using System.Text;
 using System.Threading.Tasks;
+using TechnicalTest.Domain.Interfaces;
 
 namespace TechnicalTest.Domain.Strategies
 {
@@ -12,18 +13,20 @@ namespace TechnicalTest.Domain.Strategies
     {
 
         private readonly ILogger<FileOutputStrategy> _logger;
+        private readonly IFileService _fileService;
         private readonly ApplicationParameters _settings;
 
-        public FileOutputStrategy(ILogger<FileOutputStrategy> logger, IOptions<ApplicationParameters> settings)
+        public FileOutputStrategy(ILogger<FileOutputStrategy> logger, IOptions<ApplicationParameters> settings, IFileService fileService)
         {
             _logger = logger;
             _settings = settings.Value;
+            _fileService = fileService;
         }
 
         public override async Task OutputData(List<string> wordLadder)
         {
             _logger.LogInformation($"Writing results to {_settings.OutputFile}");
-            await File.WriteAllLinesAsync(_settings.OutputFile, wordLadder);
+            await _fileService.WriteResutsToFile(wordLadder, _settings.OutputFile);
         }
     }
 }
